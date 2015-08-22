c5ccb4c72fb920e2d0ce958005193220868f01ab
Sean Moriarty <sean.moriarty@gopassport.com>
2015-08-22 14:39:02 -0400

    Pushing assets and script creation for objects

---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Object : MonoBehaviour {
+	public int price;
+	public enum object_type { knife, chocolate };
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PickupObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObject : Object {
+	public int max_hp, hp;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	int get_max_hp() {
+		return this.max_hp;
+	}
+
+	void set_hp(int new_hp) {
+		this.hp = new_hp;
+	}
+
+	int get_hp() {
+		return this.hp;
+	}
+
+}
