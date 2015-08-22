31b5a99a3d52b74ff8ae654a6e8d043222fe45ce
Sean Moriarty <sean.moriarty@gopassport.com>
2015-08-22 15:32:41 -0400

    Adding my scene

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chest : Object {
+	private bool chest_opened;
+
+	void Start () {
+		chest_opened = false;
+	}
+
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		Debug.Log("I clicked that shit");
+	}
+}
--- Assets/Scripts/Object.cs
+++ Assets/Scripts/Object.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class Object : MonoBehaviour {
 	public int price;
-	public enum object_type { knife, chocolate };
 
 	void Start () {
 	
